811015f0f9b54fe483974f1bf92acec5d96559c0
Vutlapallianusree <vutlapallianusree@gmail.com>
2026-02-08 12:27:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <h1> Hello, This Is Navya Sree</h1>
+    <p> I am Navya Sree , studing B-Tech final year at Bhoj Reddy Engineering College for Women in the field of Information Technology . 
+      My native place is Andhar Pradesh, and I am currently settled in Hyderabad , bandlaguda jagir .. </p>
+      <button>Click To Know</button>
+      <h3> My Education Background</h3>
+      <ul> 
+        <li> 10th : Rau's High School with 9.7 cgpa</li>
+        <li> Diploma : Government Polytechnic For Women Secunderabad with 9.7 cgpa</li>
+        <li> B-Tech : Bhoj Reddy Engineering College for Women with 8.6 cgpa </li>
+      </ul>
+    </> 
+  )
+}
+
+export default App
